a50aeb4f32b5c4e1c8744ccaa97f319a5ba4510e
Shellsea <elainesbarrera@gmail.com>
2020-11-24 16:17:18 -0800

    ADDED: inquirer, created questions, prompt on init function

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-const util = require("./utils/generateMarkdown")
-util.generateMarkdown("hello")
+// const util = require("./utils/generateMarkdown");
+// util.generateMarkdown(titlefromdata)
+
+const inquirer = require("inquirer");
 
 // array of questions for user
 const questions = [
-
+  "What is the title of your project?",
+  "Write a description for your project",
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+const init = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Write a description for your project",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+    });
+};
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,8 @@
 module.exports = {
   generateMarkdown: // function to generate markdown for README
   function generateMarkdown(data) {
-    console.log(data)
-  //   return `# ${data.title}
+    return `# ${data.title}
   
-  // `;
+  `;
   }
 };
