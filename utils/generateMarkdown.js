636c35dd7e795d4289c010e7b3538b06b0b0d99f
Shellsea <elainesbarrera@gmail.com>
2020-11-30 14:19:43 -0800

    FIXED: github link in function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,30 +4,34 @@ module.exports = {
   generateMarkdown: function generateMarkdown(answer) {
     return `# ${answer.title}
   
+## License
+${answer.license}
+
 ## Description
-      ${answer.description}
-      ## Table of Contents
-      1. Installation(#installation)
-      2. Usage(#usage)
-      3. Contributing(#contributing)
-      4. Tests(#tests)
-      5. Questions(#questions)
-      6. License(#licence)
+${answer.description}
+
+## Table of Contents
+  1. Installation(#installation)
+  2. Usage(#usage)
+  3. Contributing(#contributing)
+  4. Tests(#tests)
+  5. Questions(#questions)
+  6. License(#licence)
   
-      ## Installation
-      ${answer.install}
-      ## Usage
-      ${answer.usage}
-      ## Contributing
-      ${answer.contribute}
-      ## Tests
+## Installation
+${answer.install}
+
+## Usage
+${answer.usage}
+
+## Contributing
+${answer.contribute}
+
+## Tests
   
-      ## Questions
-      If you have additional questions send me an [email](${answer.email}).
-      You can also visit my GitHub account for more projects by clicking [here](${answer.username}).
-  
-      ## License
-      ${answer.license}
+## Questions
+If you have additional questions send me an [email](${answer.email}).
+You can also visit my GitHub account for more projects by clicking [here](https://github.com/${answer.username}).
   
   `;
   },
